84e51a77a235f3f37d040df806a836bc032fd94e
delta-kor <deltakbot1@gmail.com>
2020-12-17 11:25:59 +0900

    Add user interface, model

---
+++ src/modules/user/user.model.ts
@@ -0,0 +1,22 @@
+import { Document, Schema, model, Model } from 'mongoose';
+import { v4 as uuid } from 'uuid';
+import User from './user.interface';
+
+export interface UserModel {
+  emailExists(email: string): Promise<boolean>;
+}
+
+const UserSchema = new Schema<User>({
+  uuid: { type: String, required: true, unique: true, default: () => uuid },
+  nickname: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+});
+
+UserSchema.static('emailExists', async (email: string) => {
+  const user = await UserModel.find({ email }).exec();
+  return !!user;
+});
+
+const UserModel = model<User & Document, UserModel & Model<User & Document>>('User', UserSchema);
+export default UserModel;
