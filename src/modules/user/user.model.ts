f3b6a1b77da150d19746a12d5d99cfaaaaf277e0
delta-kor <deltakbot1@gmail.com>
2020-12-17 13:37:38 +0900

    Fix default value

--- src/modules/user/user.model.ts
+++ src/modules/user/user.model.ts
@@ -7,16 +7,19 @@ export interface UserModel {
 }
 
 const UserSchema = new Schema<User>({
-  uuid: { type: String, required: true, unique: true, default: () => uuid },
+  uuid: { type: String, required: true, unique: true, default: () => uuid() },
   nickname: { type: String, required: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
 });
 
-UserSchema.static('emailExists', async (email: string) => {
-  const user = await UserModel.find({ email }).exec();
-  return !!user;
-});
+UserSchema.static(
+  'emailExists',
+  async (email: string): Promise<boolean> => {
+    const user = await UserModel.find({ email }).exec();
+    return !!user.length;
+  }
+);
 
 const UserModel = model<User, UserModel & Model<User>>('User', UserSchema);
 export default UserModel;
