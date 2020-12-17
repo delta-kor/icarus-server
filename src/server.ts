ad9a4783809e280bcab78015d45f16c965a1f82e
delta-kor <deltakbot1@gmail.com>
2020-12-17 11:41:57 +0900

    Add controllers mounting

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,8 @@
 import 'dotenv/config';
 import App from './app';
+import UserController from './modules/user/user.controller';
 
 const port = parseInt(process.env.PORT || '3000');
 
-const app = new App(port, []);
+const app = new App(port, [new UserController()]);
 app.listen();
