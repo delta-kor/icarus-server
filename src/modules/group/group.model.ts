4cb30d7c124d7e4faab6039198e9dcac92e050e7
delta-kor <deltakbot1@gmail.com>
2020-12-23 11:05:50 +0900

    Add group model

---
+++ src/modules/group/group.model.ts
@@ -0,0 +1,14 @@
+import { model, Schema } from 'mongoose';
+import Group from './group.interface';
+import uuid from '../../utils/uuid.util';
+import { UserSchema } from '../user/user.model';
+
+const GroupSchema = new Schema<Group>({
+  uuid: { type: String, required: true, unique: true, default: () => uuid(8) },
+  name: { type: String, required: true },
+  admin: { type: UserSchema, required: true },
+  manager: { type: [UserSchema], required: true, default: [] },
+});
+
+const GroupModel = model<Group>('group', GroupSchema);
+export default GroupModel;
