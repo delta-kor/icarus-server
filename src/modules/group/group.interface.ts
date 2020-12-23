d166d80906c1bb1c2b5ec3cc7fbdaeef8e852361
delta-kor <deltakbot1@gmail.com>
2020-12-23 11:02:27 +0900

    Add group module

---
+++ src/modules/group/group.interface.ts
@@ -0,0 +1,9 @@
+import { Document } from 'mongoose';
+import User from '../user/user.interface';
+
+export default interface Group extends Document {
+  uuid: string;
+  name: string;
+  admin: User;
+  manager: User[];
+}
