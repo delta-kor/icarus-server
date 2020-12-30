7d3a424c59a057fe0bc684c0c5414bf5cc7e9fa1
delta-kor <deltakbot1@gmail.com>
2020-12-30 18:18:54 +0900

    Add group controller

---
+++ src/modules/group/group.controller.ts
@@ -0,0 +1,7 @@
+import Controller from '../../types/controller.class';
+
+export default class GroupController extends Controller {
+  public path: string = '/group';
+
+  protected mountRoutes() {}
+}
