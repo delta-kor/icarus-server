64c933024fb5dde1d7e54eba35552cef403765a8
delta-kor <deltakbot1@gmail.com>
2021-01-23 14:30:02 +0900

    Add post content interface

--- src/modules/post/post.interface.ts
+++ src/modules/post/post.interface.ts
@@ -5,10 +5,32 @@ export enum PostType {
   TIMELINE = 'timeline',
 }
 
+export enum MentionType {
+  USER = 'user',
+  GROUP = 'group',
+}
+
+export interface PostContent {
+  text: string;
+  attachments: PostAttachment[];
+}
+
+export interface PostAttachment {}
+
+export interface ImageAttachment extends PostAttachment {
+  key: string;
+}
+
+export interface MentionAttachment extends PostAttachment {
+  from: number;
+  length: number;
+  type: MentionType;
+}
+
 export default interface Post extends Document {
   uuid: string;
   author: string;
   type: PostType;
   target: string;
-  content: any;
+  content: PostContent;
 }
